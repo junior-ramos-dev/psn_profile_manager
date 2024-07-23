6184548b78e197579aef2eee9deba4014a208dbf
JRamos29 <malebolge@gmail.com>
2024-07-23 12:41:04 -0300

    Included CSP directives

--- src/index.tsx
+++ src/index.tsx
@@ -9,26 +9,42 @@ import { AppThemeProvider } from "./theme/AppThemeProvider";
 
 import App from "./App";
 
-import { APP_TITLE, APP_DESCRIPTION } from "./utils/constants";
-
-const root = createRoot(document.getElementById("root") as HTMLElement);
+const root = createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
     <HelmetProvider>
       <Helmet>
-        <title>{APP_TITLE}</title>
-        <meta name="description" content={APP_DESCRIPTION} />
+        <meta
+          httpEquiv="Content-Security-Policy"
+          content="default-src 'self'; 
+                  connect-src 'self' http://127.0.0.1:8001 http://localhost:8001 http://localhost:3000 https://image.api.playstation.com https://images.unsplash.com;
+                  style-src 'self' https://fonts.googleapis.com https://unpkg.com https://localhost:8001 'unsafe-inline';
+                  script-src 'self' https://fonts.googleapis.com https://unpkg.com 'unsafe-inline' 'unsafe-eval';
+                  font-src 'self' data: https://fonts.gstatic.com https://unpkg.com;
+                  img-src 'self' https://image.api.playstation.com;"
+        />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
         />
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
+        />
       </Helmet>
       <Provider store={store}>
-        {/* 4. Wrap Theme provider with MUI Styled engine */}
+        {/* Wrap Theme provider with MUI Styled engine */}
         <StyledEngineProvider injectFirst>
-          {/* 5. Wrap your app with the Theme Provider */}
+          {/* Wrap your app with the Theme Provider */}
           <AppThemeProvider>
             <App />
           </AppThemeProvider>
