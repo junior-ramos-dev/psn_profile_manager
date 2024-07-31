a8802eea7e2d93840b39cdd1a5234f797792bc5a
JRamos29 <malebolge@gmail.com>
2024-07-31 00:12:45 -0300

    Changed getErrorMessage function;

--- src/utils/restApi.ts
+++ src/utils/restApi.ts
@@ -6,18 +6,19 @@ export enum VERBS {
   DELETE = "DELETE",
 }
 
-enum HTTP_STATUS_ERROR {
+export enum HTTP_STATUS_ERROR {
   CLIENT_ERROR = "Client Bad Request!",
   SERVER_ERROR = "Server Error!",
   NOT_FOUND_ERROR = "Not Found Error!",
+  NETWORK_ERROR = "Unable to Communicate with Server via HTTP(s)",
   UNKNOWN_ERROR = "Unknown Error. Something Went Wrong!",
   NO_ERROR = "",
 }
 
 const getStatusMessage = (status: number, message: string) =>
-  `${status} + ": "+ ${message}`;
+  `HTTP(s) ${status}: ${message}`;
 
-export const getErrorMessage = (status = 403) => {
+export const getErrorMessage = (status) => {
   let range = 0;
 
   if (!status) range = 1;
@@ -28,7 +29,7 @@ export const getErrorMessage = (status = 403) => {
 
   switch (range) {
     case 1:
-      return getStatusMessage(status, HTTP_STATUS_ERROR.UNKNOWN_ERROR);
+      return getStatusMessage(0, HTTP_STATUS_ERROR.NETWORK_ERROR);
     case 2:
       return getStatusMessage(status, HTTP_STATUS_ERROR.NO_ERROR);
     case 3:
