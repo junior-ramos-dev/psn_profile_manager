763d476b1680607afadf1298c51afffdb0547656
JRamos29 <malebolge@gmail.com>
2024-07-31 18:54:27 -0300

    Modified htto error messsages;

--- src/utils/restApi.ts
+++ src/utils/restApi.ts
@@ -10,7 +10,7 @@ export enum HTTP_STATUS_ERROR {
   CLIENT_ERROR = "Client Bad Request!",
   SERVER_ERROR = "Server Error!",
   NOT_FOUND_ERROR = "Not Found Error!",
-  NETWORK_ERROR = "Unable to Communicate with Server via HTTP(s)",
+  SERVICE_UNAVAILABLE = "Service Unavailable. Unable to Communicate with Server!",
   UNKNOWN_ERROR = "Unknown Error. Something Went Wrong!",
   NO_ERROR = "",
 }
@@ -21,24 +21,40 @@ const getStatusMessage = (status: number, message: string) =>
 export const getErrorMessage = (status) => {
   let range = 0;
 
-  if (!status) range = 1;
-  if (status < 300) range = 2;
-  if (status === 400 && status < 404) range = 3;
-  if (status === 404 && status < 500) range = 4;
-  if (status >= 500) range = 5;
+  console.log(status);
+
+  if (status < 300) range = 1;
+  if (status === 400 && status < 403) range = 2;
+  if (status === 404 && status < 500) range = 3;
+  if (status === 503) range = 4;
+  if (status !== 503 && status >= 500) range = 5;
+
+  let errorMessage = "";
 
   switch (range) {
     case 1:
-      return getStatusMessage(0, HTTP_STATUS_ERROR.NETWORK_ERROR);
+      errorMessage = getStatusMessage(status, HTTP_STATUS_ERROR.NO_ERROR);
+      break;
     case 2:
-      return getStatusMessage(status, HTTP_STATUS_ERROR.NO_ERROR);
+      errorMessage = getStatusMessage(status, HTTP_STATUS_ERROR.CLIENT_ERROR);
+      break;
     case 3:
-      return getStatusMessage(status, HTTP_STATUS_ERROR.CLIENT_ERROR);
+      errorMessage = getStatusMessage(
+        status,
+        HTTP_STATUS_ERROR.NOT_FOUND_ERROR
+      );
+      break;
     case 4:
-      return getStatusMessage(status, HTTP_STATUS_ERROR.NOT_FOUND_ERROR);
+      errorMessage = getStatusMessage(
+        status,
+        HTTP_STATUS_ERROR.SERVICE_UNAVAILABLE
+      );
+      break;
     case 5:
-      return getStatusMessage(status, HTTP_STATUS_ERROR.SERVER_ERROR);
+      errorMessage = getStatusMessage(status, HTTP_STATUS_ERROR.SERVER_ERROR);
+      break;
     default:
-      return getStatusMessage(status, HTTP_STATUS_ERROR.UNKNOWN_ERROR);
+      errorMessage = getStatusMessage(status, HTTP_STATUS_ERROR.UNKNOWN_ERROR);
   }
+  return errorMessage;
 };
