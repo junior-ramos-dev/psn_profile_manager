5e1de1a2b91dfc9c3eff0925d7bef421b6c838f0
JRamos29 <malebolge@gmail.com>
2024-08-02 20:06:53 -0300

    Revert changes;

--- src/services/rtkQueryApi/games/gamesSelectors.ts
+++ src/services/rtkQueryApi/games/gamesSelectors.ts
@@ -1,10 +1,3 @@
-import { RootState } from "@/store";
-import { IGamesInitialState } from "./gamesSlice";
-
-const getGamesState = (state: RootState) => state.games as IGamesInitialState;
-
-export const getGamesList = (state: RootState) =>
-  getGamesState(state).gamesList;
-export const getGamesRoutesList = (state: RootState) =>
-  getGamesState(state).gamesRoutesList;
-export const getGamesEtag = (state: RootState) => getGamesState(state).eTag;
+export const getGamesList = (state) => state.games.gamesList;
+export const getGamesRoutesList = (state) => state.games.gamesRoutesList;
+export const getGamesEtag = (state) => state.games.eTag;
