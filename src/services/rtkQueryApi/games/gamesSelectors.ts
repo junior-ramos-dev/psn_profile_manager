7afde0fce57fddc4e799106fe814721666fe55a0
JRamos29 <malebolge@gmail.com>
2024-08-02 12:27:29 -0300

    Typed gamesSelectors;

--- src/services/rtkQueryApi/games/gamesSelectors.ts
+++ src/services/rtkQueryApi/games/gamesSelectors.ts
@@ -1,2 +1,10 @@
-export const getGamesList = (state) => state.games.gamesList;
-export const getGamesRoutesList = (state) => state.games.gamesRoutesList;
+import { RootState } from "@/store";
+import { IGamesInitialState } from "./gamesSlice";
+
+const getGamesState = (state: RootState) => state.games as IGamesInitialState;
+
+export const getGamesList = (state: RootState) =>
+  getGamesState(state).gamesList;
+export const getGamesRoutesList = (state: RootState) =>
+  getGamesState(state).gamesRoutesList;
+export const getGamesEtag = (state: RootState) => getGamesState(state).eTag;
