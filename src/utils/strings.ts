c54ae50774eb057a7c80618b45a3218e720536cd
JRamos29 <malebolge@gmail.com>
2024-08-05 18:51:02 -0300

    Changed StringUtils class;

--- src/utils/strings.ts
+++ src/utils/strings.ts
@@ -1,17 +1,15 @@
+import _ from "lodash";
+
 export class StringUtils {
   //Compare two strings case insensitive
   public static compareStringIgnoreCase(str1: string, str2: string) {
-    return str1.toUpperCase() === str2.toUpperCase();
+    return _.toLower(str1) === _.toLower(str2);
   }
 
   //Format string to capitalize the first letter for each word
   public static formatStringToTitleCase(str: string) {
-    const toLowerCase = str.toLowerCase();
-
-    const toTitleCase = toLowerCase.replace(
-      /(^\w|\s\w)(\S*)/g,
-      (_: any, m1: string, m2: string) => m1.toUpperCase() + m2.toLowerCase()
-    );
+    const toLowerCase = _.toLower(str);
+    const toTitleCase = _.startCase(toLowerCase);
 
     return toTitleCase;
   }
