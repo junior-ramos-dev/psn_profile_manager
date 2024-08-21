f503f4eb80033f8538a115ae3613997245e58112
JRamos29 <malebolge@gmail.com>
2024-08-21 10:46:20 -0300

    Updated auth endpoints;

--- src/services/rtkQueryApi/auth/authApi.ts
+++ src/services/rtkQueryApi/auth/authApi.ts
@@ -8,33 +8,41 @@ import { VERBS } from "@/utils/http";
 
 import { rtkQueryBaseApi } from "../rtkQueryBaseApi";
 
+import { AUTH_ENDPOINT_NAME, AUTH_URL_MAP } from ".";
+
 export const authApi = rtkQueryBaseApi.injectEndpoints({
   endpoints: (build) => ({
     login: build.mutation<AuthLoginResponse, AuthLoginRequest>({
       query: ({ email, password }) => ({
-        endpointUrl: "auth/login",
+        endpointUrl: AUTH_URL_MAP[AUTH_ENDPOINT_NAME.LOGIN],
         method: VERBS.POST,
         bodyData: { email, password },
         collection: "Auth",
-        endpointName: "login",
+        endpointName: AUTH_ENDPOINT_NAME.LOGIN,
+        headers: {
+          Authorization: true,
+        },
       }),
     }),
     register: build.mutation<AuthRegisterResponse, AuthRegisterRequest>({
       query: (data) => ({
-        endpointUrl: "auth/register",
+        endpointUrl: AUTH_URL_MAP[AUTH_ENDPOINT_NAME.REGISTER],
         method: VERBS.POST,
         bodyData: data,
         collection: "Auth",
-        endpointName: "register",
+        endpointName: AUTH_ENDPOINT_NAME.REGISTER,
+        headers: {
+          Authorization: true,
+        },
       }),
       invalidatesTags: ["Auth"],
     }),
     logout: build.mutation<void, void>({
       query: () => ({
-        endpointUrl: "auth/logout",
+        endpointUrl: AUTH_URL_MAP[AUTH_ENDPOINT_NAME.LOGOUT],
         method: VERBS.POST,
         collection: "Auth",
-        endpointName: "logout",
+        endpointName: AUTH_ENDPOINT_NAME.LOGOUT,
       }),
       invalidatesTags: ["Auth"],
     }),
