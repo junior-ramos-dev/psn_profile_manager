6f337a6792dd48c46e13aa971ca0dfec2a0890f0
JRamos29 <malebolge@gmail.com>
2024-09-18 21:37:36 -0300

    Updated platform icons;

--- src/components/Playstation/PsPlatformIcon.tsx
+++ src/components/Playstation/PsPlatformIcon.tsx
@@ -1,10 +1,28 @@
+import ps3Icon from "@/assets/psn/icons/ps3Icon.png";
 import ps4Icon from "@/assets/psn/icons/ps4Icon.png";
 import ps5Icon from "@/assets/psn/icons/ps5Icon.png";
+import psVitaIcon from "@/assets/psn/icons/psVitaIcon.png";
 
 interface IPsPlatformIconProps {
   width?: number;
   height?: number;
 }
+
+// PS5 Icon
+export const Ps5PlatformIcon = ({
+  width = 30,
+  height = 12,
+}: IPsPlatformIconProps) => (
+  <img
+    alt="Level"
+    src={ps5Icon}
+    width={width}
+    height={height}
+    style={{
+      borderRadius: "5%",
+    }}
+  />
+);
 
 // PS4 Icon
 export const Ps4PlatformIcon = ({
@@ -22,14 +40,30 @@ export const Ps4PlatformIcon = ({
   />
 );
 
-// PS5 Icon
-export const Ps5PlatformIcon = ({
+// PS3 Icon
+export const Ps3PlatformIcon = ({
   width = 30,
-  height = 12,
+  height = 13,
 }: IPsPlatformIconProps) => (
   <img
     alt="Level"
-    src={ps5Icon}
+    src={ps3Icon}
+    width={width}
+    height={height}
+    style={{
+      borderRadius: "5%",
+    }}
+  />
+);
+
+// PS Vita Icon
+export const PsVitaPlatformIcon = ({
+  width = 30,
+  height = 13,
+}: IPsPlatformIconProps) => (
+  <img
+    alt="Level"
+    src={psVitaIcon}
     width={width}
     height={height}
     style={{
@@ -51,8 +85,15 @@ export const getTrophyIconByPlatfrom = (
   width?: number,
   height?: number
 ) => {
-  if (platfrom.toLowerCase().includes("ps4"))
-    return <Ps4PlatformIcon width={width} height={height} />;
-  if (platfrom.toLowerCase().includes("ps5"))
+  if (platfrom.toLowerCase().startsWith("ps5"))
     return <Ps5PlatformIcon width={width} height={height} />;
+  if (platfrom.toLowerCase().startsWith("ps4"))
+    return <Ps4PlatformIcon width={width} height={height} />;
+  if (platfrom.toLowerCase().startsWith("ps3"))
+    return <Ps3PlatformIcon width={width} height={height} />;
+  if (
+    !platfrom.toLowerCase().startsWith("ps3") &&
+    platfrom.toLowerCase().includes("vita")
+  )
+    return <PsVitaPlatformIcon width={width} height={height} />;
 };
