9214aad8d9660a7a6f2d207cbf7ad49ecf908044
JRamos29 <malebolge@gmail.com>
2024-09-28 16:57:26 -0300

    Implemented LinarBuffer component;

---
+++ src/components/Common/LinearBuffer.tsx
@@ -0,0 +1,42 @@
+import * as React from "react";
+import { useEffect, useRef, useState } from "react";
+
+import Box from "@mui/material/Box";
+import LinearProgress from "@mui/material/LinearProgress";
+
+export const LinearBuffer = () => {
+  const [progress, setProgress] = useState(0);
+  const [buffer, setBuffer] = useState(10);
+
+  const progressRef = useRef(() => {});
+  useEffect(() => {
+    progressRef.current = () => {
+      if (progress === 100) {
+        setProgress(0);
+        setBuffer(10);
+      } else {
+        setProgress(progress + 1);
+        if (buffer < 100 && progress % 5 === 0) {
+          const newBuffer = buffer + 1 + Math.random() * 10;
+          setBuffer(newBuffer > 100 ? 100 : newBuffer);
+        }
+      }
+    };
+  });
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      progressRef.current();
+    }, 100);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      <LinearProgress variant="buffer" value={progress} valueBuffer={buffer} />
+    </Box>
+  );
+};
