8be71bebc9d00708163efb7f2342b9c4441222f0
JRamos29 <malebolge@gmail.com>
2024-09-28 17:01:18 -0300

    Implemented task loaders;

---
+++ src/services/taskLoaders/auth/authTaskLoaders.ts
@@ -0,0 +1,97 @@
+import { ITaskProps } from "@/models/axios/taskLoader";
+import { AuthRegisterRequest } from "@/models/types/rtkQuery/auth";
+import { authApi } from "@/services/rtkQueryApi/auth/authApi";
+import { VERBS } from "@/settings/app/constants";
+import {
+  AUTH_ENDPOINT_NAME,
+  AUTH_URL_MAP,
+} from "@/settings/app/constants/api/auth";
+
+import { BaseTaskLoader } from "../baseTaskLoader";
+
+// The number of steps for the task
+export enum STEP_ID {
+  ONE = 1,
+  TWO = 2,
+  THREE = 3,
+  FOUR = 4,
+  FIVE = 5,
+  SIX = 6,
+  SEVEN = 7,
+  EIGHT = 8,
+  NINE = 9,
+}
+
+// Return the message for each step
+export const STEP_MSG_MAP: Record<STEP_ID, string> = {
+  [STEP_ID.ONE]: "Checking PSN On-Line ID...",
+  [STEP_ID.TWO]: "Checking E-Mail...",
+  [STEP_ID.THREE]: "Checking NPSSO code...",
+  [STEP_ID.FOUR]: "Getting PSN credentials...",
+  [STEP_ID.FIVE]: "Creating Account...",
+  [STEP_ID.SIX]: "Getting User games list...",
+  [STEP_ID.SEVEN]: "Loading games icons...",
+  [STEP_ID.EIGHT]: "Getting games trophy lists...",
+  [STEP_ID.NINE]: "Finishing...",
+};
+
+export class AuthTaskLoader extends BaseTaskLoader {
+  // queryString = `?runTask=${this.runTask}&runSubTask=${this.runSubTask}&stepId=${this.stepId}`;
+
+  loadData = async () => {
+    const registerData = await this.baseLoader(
+      authApi.endpoints.registerLoader,
+      this.loaderQuery
+    );
+
+    return registerData;
+  };
+
+  initAuthregisterLoaderQuery = (authRegisterRequest: AuthRegisterRequest) => {
+    this.loaderQuery = {
+      endpointUrl: AUTH_URL_MAP[AUTH_ENDPOINT_NAME.REGISTER_LOADER],
+      method: VERBS.POST,
+      bodyData: authRegisterRequest,
+      collection: "Auth",
+      endpointName: AUTH_ENDPOINT_NAME.REGISTER_LOADER,
+    };
+  };
+
+  handleStepsParams = (taskProps: ITaskProps) => {
+    if (taskProps.stepId === STEP_ID.ONE && taskProps.runSubTask === 0) {
+      // this.stepId = STEP_ID.TWO;
+      this.runSubTask = 1;
+    }
+
+    if (taskProps.stepId === STEP_ID.TWO && taskProps.runSubTask === 0) {
+      this.runSubTask = 1;
+    }
+
+    if (taskProps.stepId === STEP_ID.THREE && taskProps.runSubTask === 0) {
+      this.runSubTask = 1;
+    }
+    if (taskProps.stepId === STEP_ID.FOUR && taskProps.runSubTask === 0) {
+      this.runSubTask = 1;
+    }
+
+    if (taskProps.stepId === STEP_ID.FIVE && taskProps.runSubTask === 0) {
+      this.runSubTask = 1;
+    }
+
+    if (taskProps.stepId === STEP_ID.SIX && taskProps.runSubTask === 0) {
+      this.runSubTask = 1;
+    }
+
+    if (taskProps.stepId === STEP_ID.SEVEN && taskProps.runSubTask === 0) {
+      this.runSubTask = 1;
+    }
+
+    if (taskProps.stepId === STEP_ID.EIGHT && taskProps.runSubTask === 0) {
+      this.runSubTask = 1;
+    }
+
+    if (taskProps.stepId === STEP_ID.NINE && taskProps.runSubTask === 0) {
+      this.runSubTask = 1;
+    }
+  };
+}
---
+++ src/services/taskLoaders/baseTaskLoader.ts
@@ -0,0 +1,49 @@
+import { ITaskHandler, ITaskProps } from "@/models/axios/taskLoader";
+import { IAxiosBaseQueryArgs } from "@/services/axios/axiosBaseQueryApi";
+import { AUTH_ENDPOINT_NAME } from "@/settings/app/constants/api/auth";
+import { AppDispatch, AppStore } from "@/store";
+
+export abstract class BaseTaskLoader {
+  store: AppStore;
+  dispatch: AppDispatch;
+  loaderQuery: IAxiosBaseQueryArgs;
+  runTask: number = 0;
+  runSubTask: number = 0;
+  stepId: number = 1;
+  constructor(store: AppStore) {
+    this.store = store;
+    this.dispatch = store.dispatch;
+  }
+
+  baseLoader = async (endpoint, loaderQuery: IAxiosBaseQueryArgs) => {
+    const promise = this.dispatch(endpoint.initiate(loaderQuery));
+
+    // if (request) request.signal.onabort = promise.abort;
+
+    const res = await promise;
+    const { data, isError, error } = res;
+
+    if (isError && error.status !== 304) {
+      const errorInfo = error;
+      if (errorInfo.status >= 400 && errorInfo.status <= 599) {
+        console.log(errorInfo.status);
+        throw new Response("", {
+          status: errorInfo.status,
+          statusText: errorInfo.data,
+        });
+      }
+    }
+    return this.handleResponseData(loaderQuery.endpointName, data);
+  };
+
+  handleResponseData = (endpointName, data) => {
+    if (!data) {
+      if (endpointName === AUTH_ENDPOINT_NAME.REGISTER_LOADER) return null;
+    }
+    return data;
+  };
+
+  abstract loadData(): Promise<ITaskHandler>;
+
+  abstract handleStepsParams(taskProps: ITaskProps): void;
+}
